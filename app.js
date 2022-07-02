df34ba775c9874a260a4bf7b8fa1448640b3ba76
Ahsan <omerjee.ahsan@gmail.com>
2022-07-02 17:59:20 +0500

    ready

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var con1=document.getElementById("tabc-1");
 var tab2=document.getElementById("tab-2");
 var con2=document.getElementById("tabc-2");
 var m=document.getElementById("modal");
+var v=document.getElementById("modal1");
 
 function tabchange(val){
     // console.log(val);
     if (val==0) {
         con0.style.display="block";
         tab0.style.borderBottom="3px solid white";
-        con0.style.animation="ani 2s";
+        con0.style.animation="ani 1s";
         con1.style.display="none";
         con2.style.display="none";
         tab1.style.borderBottom="none";
@@ -24,7 +25,7 @@ function tabchange(val){
         con0.style.display="none";
         con1.style.display="block";
         tab1.style.borderBottom="3px solid white";
-        con1.style.animation="ani 2s";
+        con1.style.animation="ani 1s";
         tab0.style.borderBottom="none";
         tab2.style.borderBottom="none";
         con2.style.display="none";
@@ -36,13 +37,22 @@ function tabchange(val){
         tab1.style.borderBottom="none";
         con2.style.display="block";
         tab2.style.borderBottom="3px solid white";
-        con2.style.animation="ani 2s";
+        con2.style.animation="ani 1s";
     }
 
 }
 
-function modalchange() {
-    m.style.display="block";
+function modalchange(a) {
+
+    if (a==0) {
+        m.style.display="block";
+        m.style.animation="ani 1s";
+    }
+    if (a==1) {
+        v.style.display="block";
+        v.style.animation="ani 1s";
+    }
+    
 }
 
 function close() {
